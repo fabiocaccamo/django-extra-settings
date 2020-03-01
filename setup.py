9a9bc0415669ebc8a469322ce120757516bcd613
Fabio Caccamo <fabio.caccamo@gmail.com>
2020-03-01 12:55:47 +0100

    Added python 2.7 to tox and travis confg.

--- setup.py
+++ setup.py
@@ -54,6 +54,8 @@ setup(
         'License :: OSI Approved :: MIT License',
         'Natural Language :: English',
         'Operating System :: OS Independent',
+        'Programming Language :: Python :: 2',
+        'Programming Language :: Python :: 2.7',
         'Programming Language :: Python :: 3',
         'Programming Language :: Python :: 3.4',
         'Programming Language :: Python :: 3.5',
