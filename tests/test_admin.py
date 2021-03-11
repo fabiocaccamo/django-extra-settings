7d889b31b6f567453b0c9fca0a374ed7a5083c2d
Almas Baktubayev <a.baktubayev@gmail.com>
2021-03-11 11:15:41 +0600

    disable warning message - sidebar

--- tests/test_admin.py
+++ tests/test_admin.py
@@ -1,5 +1,7 @@
 # -*- coding: utf-8 -*-
 
+from django.contrib import admin
+
 from django.contrib.admin.sites import AdminSite
 from django.test import TestCase
 
@@ -7,27 +9,33 @@ from extra_settings.admin import SettingAdmin
 from extra_settings.forms import SettingForm
 from extra_settings.models import Setting
 
+# Disable warning message
+admin.autodiscover()
+admin.site.enable_nav_sidebar = False
+
 
 class MockRequest(object):
     pass
 
+
 class MockSuperUser(object):
     def has_perm(self, perm):
         return True
+
 
 request = MockRequest()
 request.user = MockSuperUser()
 
 
 class ExtraSettingsAdminTestCase(TestCase):
-
     def setUp(self):
         self._setting_obj, setting_created = Setting.objects.get_or_create(
             name='PACKAGE_NAME',
             defaults={
-                'value_type':Setting.TYPE_STRING,
-                'value_string':'django-extra-settings',
-            })
+                'value_type': Setting.TYPE_STRING,
+                'value_string': 'django-extra-settings',
+            },
+        )
         self._site = AdminSite()
 
     def tearDown(self):
@@ -39,10 +47,14 @@ class ExtraSettingsAdminTestCase(TestCase):
 
     def test_fieldsets(self):
         ma = SettingAdmin(model=Setting, admin_site=AdminSite())
-        self.assertEqual(ma.get_fieldsets(request),
-            ((None, {'classes': ('wide',), 'fields': ('name', 'value_type')}),) )
-        self.assertEqual(ma.get_fieldsets(request, self._setting_obj),
-            ((None, {'classes': ('wide',), 'fields': ('name', 'value_type', 'value_string')}),) )
+        self.assertEqual(ma.get_fieldsets(request), ((None, {
+            'classes': ('wide', ),
+            'fields': ('name', 'value_type')
+        }), ))
+        self.assertEqual(ma.get_fieldsets(request, self._setting_obj), ((None, {
+            'classes': ('wide', ),
+            'fields': ('name', 'value_type', 'value_string')
+        }), ))
 
     def test_modeladmin_save(self):
         ma = SettingAdmin(model=Setting, admin_site=AdminSite())
