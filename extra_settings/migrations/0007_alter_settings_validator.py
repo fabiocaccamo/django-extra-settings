b93f033d105767e8f8f6a987f5c981c750cbee23
Domenico Nappo <domenico.nappo@aidence.com>
2022-09-27 08:57:25 +0200

    Resolve #33 add missing migration

---
+++ extra_settings/migrations/0007_alter_settings_validator.py
@@ -0,0 +1,19 @@
+from django.db import migrations, models
+
+
+class Migration(migrations.Migration):
+    dependencies = [
+        ('extra_settings', '0006_setting_validator'),
+    ]
+
+    operations = [
+        migrations.AlterField(
+            model_name='setting',
+            name='validator',
+            field=models.CharField(
+                blank=True,
+                help_text="Full python path to a validator function, eg. 'myapp.mymodule.my_validator'",
+                max_length=255, null=True, verbose_name='Validator'
+            ),
+        ),
+    ]
