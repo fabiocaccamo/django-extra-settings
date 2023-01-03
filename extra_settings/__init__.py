662befa8bd02aa51dd418d9d5166a03af7fc2c34
Fabio Caccamo <fabio.caccamo@gmail.com>
2023-01-03 12:21:06 +0100

    Fix `settings` not configured yet when reading version attr from `setup.cfg`.

--- extra_settings/__init__.py
+++ extra_settings/__init__.py
@@ -1,3 +1,8 @@
-from extra_settings import settings
+from django.core.exceptions import ImproperlyConfigured
+
+try:
+    from extra_settings import settings
+except ImproperlyConfigured:
+    pass
 
 default_app_config = "extra_settings.apps.ExtraSettingsConfig"
