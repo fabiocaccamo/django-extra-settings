370d6f37a5a6ab9caf5c2b5e8b357552c36a0ede
Fabio Caccamo <fabio.caccamo@gmail.com>
2023-02-09 19:11:48 +0100

    Add `metadata` module and read package attrs dynamically.

--- extra_settings/__init__.py
+++ extra_settings/__init__.py
@@ -1,6 +1,25 @@
 from django.core.exceptions import ImproperlyConfigured
 
+from extra_settings.metadata import (
+    __author__,
+    __copyright__,
+    __description__,
+    __license__,
+    __title__,
+    __version__,
+)
+
 try:
     from extra_settings import settings  # noqa: F401
 except ImproperlyConfigured:
     pass
+
+
+__all__ = [
+    "__author__",
+    "__copyright__",
+    "__description__",
+    "__license__",
+    "__title__",
+    "__version__",
+]
---
+++ extra_settings/metadata.py
@@ -0,0 +1,7 @@
+__author__ = "Fabio Caccamo"
+__copyright__ = "Copyright (c) 2020-present Fabio Caccamo"
+__description__ = "config and manage typed extra settings using just the django admin."
+__email__ = "fabio.caccamo@gmail.com"
+__license__ = "MIT"
+__title__ = "django-extra-settings"
+__version__ = "0.8.1"
